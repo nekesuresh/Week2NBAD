727d2b989e34409c233ccec703ec1c203772501b
Neketha Suresh <88456215+nekesuresh@users.noreply.github.com>
2024-09-02 00:08:52 -0400

    Add files via upload

---
+++ uncc.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC-Neketha Suresh</title>
+</head>
+<body>
+    <h1> Hi, I am testing a page  </h1>
+    <hr>
+    <?php echo 9 * 13 ?>
+</body>
+</html>
